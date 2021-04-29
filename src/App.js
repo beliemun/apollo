f33c8bccefbbe0755c799b597e18f53ea843ccb9
beliemun <beliemun@paran.com>
2021-04-29 15:16:18 +0900

    lecture - 26%

--- src/App.js
+++ src/App.js
@@ -1,10 +1,16 @@
-import './App.css';
+import React from "react";
+import { Reset } from "styled-reset";
+import { HashRouter as Router, Route, Switch, } from "react-router-dom";
+import Detail from "./routes/Detail";
+import Home from "./routes/Home";
 
-function App() {
-  return (
-    <div className="App">
-    </div>
-  );
-}
+const App = () =>
+  <>
+    <Reset />
+    <Router>
+      <Route exact path="/" component={Home} />
+      <Route path="/:id" component={Detail} />
+    </Router>
+  </>
 
 export default App;
